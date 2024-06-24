80cec6a8d1412960eef05e32dd29039a86561ce4
NEYAZ NAFIZ <neyaz@aintron.com>
2024-06-25 01:50:28 +0600

    outfile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use std::fs;
+use std::io;
+use std::env::args;
+
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+}
+
+fn extracor() -> i32 {
+  let args: Vec<_> = args().collect();
+
+  if args.len() < 2 {
+    println!("Usage: {} <filename>", args[0]);
+    return 1;
+  }
+
+  let filename = std::path::Path::new(&*args[1]);
+  let file = fs::File::open(&filename).unwrap();
+  let archive = zip::ZipArchive::new(file).unwrap();
+
+  for i: i32 in 0..archive.len() {
+    let mut file = archive.by_index(i).unwrap();
+
+    let outpath = match file.enclosed_name() {
+        Some(path) => path.to_owned(),
+        None => continue,
+    };
+    {
+      let comment = file.comment();
+      if !comment.is_empty() {
+          println!("File {} comment: {}", i, comment);
+      }
+    }
+
+    if (*file.name()).ends_with('/') {
+      println!("File {} extracted to \"{}\"", i, outpath.display());
+      fs::create_dir_all(&outpath).unwrap();
+    } else {
+      println!("File {} extracted to \"{}\" ({} bytes)", i, outpath.display(), file.size);
+    };
+
+    if let Some(p) = outpath.parent() {
+      if !p.exists() {
+          fs::create_dir_all(&p).unwrap();
+      }
+    }
+
+    let mut outfile = fs::File::create(&outpath).unwrap();
+  }
 }

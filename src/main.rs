8df531ede36eacc295371498cbd57ea81775547d
NEYAZ NAFIZ <neyaz@aintron.com>
2024-06-25 02:03:07 +0600

    file permission

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env::args;
 
 
 fn main() {
-  println!("Hello, world!");
+  std::process::exit(extracor());
 }
 
 fn extracor() -> i32 {
@@ -17,9 +17,9 @@ fn extracor() -> i32 {
 
   let filename = std::path::Path::new(&*args[1]);
   let file = fs::File::open(&filename).unwrap();
-  let archive = zip::ZipArchive::new(file).unwrap();
+  let mut archive = zip::ZipArchive::new(file).unwrap();
 
-  for i: i32 in 0..archive.len() {
+  for  i in 0..archive.len() {
     let mut file = archive.by_index(i).unwrap();
 
     let outpath = match file.enclosed_name() {
@@ -37,7 +37,7 @@ fn extracor() -> i32 {
       println!("File {} extracted to \"{}\"", i, outpath.display());
       fs::create_dir_all(&outpath).unwrap();
     } else {
-      println!("File {} extracted to \"{}\" ({} bytes)", i, outpath.display(), file.size);
+      println!("File {} extracted to \"{}\" ({} bytes)", i, outpath.display(), file.size());
       
       if let Some(p) = outpath.parent() {
         if !p.exists() {
@@ -48,5 +48,15 @@ fn extracor() -> i32 {
       let mut outfile = fs::File::create(&outpath).unwrap();
       io::copy(&mut file, &mut outfile).unwrap();
     }
+
+    #[cfg(unix)]
+    {
+      use std::os::unix::fs::PermissionsExt;
+
+      if let Some(mode) = file.unix_mode() {
+        fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+      }
+    }
   }
+  return 0
 }
